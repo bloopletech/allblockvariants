3db8811c65a8e72076e037e72027794ecaba0659
Brenton Fletcher <i@bloople.net>
2025-11-06 05:17:41 +1100

    Fix missing flowerpot textures

--- src/main/java/net/bloople/allblockvariants/creators/DyedPottedContentCreator.kt
+++ src/main/java/net/bloople/allblockvariants/creators/DyedPottedContentCreator.kt
@@ -42,9 +42,7 @@ class DyedPottedContentCreator(blockInfo: BlockInfo, private val dyeColor: DyeCo
                   "parent": "$existingBlockBlockId",
                   "textures": {
                     "particle": "${MOD_ID}:block/${dyeColor.getName()}_flower_pot",
-                    "flowerpot": "${MOD_ID}:block/${dyeColor.getName()}_flower_pot",
-                    "plant": "${contentBlock.identifier.blockResourceLocation}",
-                    "sapling": "${contentBlock.identifier.blockResourceLocation}"
+                    "flowerpot": "${MOD_ID}:block/${dyeColor.getName()}_flower_pot"
                   }
                 }
             """
